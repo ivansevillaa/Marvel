2c431880c39002e0d17b87a99c5b71ebdc41f142
Ivan Sevilla <ivansevillaa@gmail.com>
2019-04-30 19:37:25 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
