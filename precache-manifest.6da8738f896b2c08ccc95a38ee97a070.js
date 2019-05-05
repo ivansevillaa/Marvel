9fe90d3e9926359a6438540c76cbb1bfdc4cc131
Ivan Sevilla <ivansevillaa@gmail.com>
2019-05-04 21:41:11 -0300

    Updates

---
+++ precache-manifest.6da8738f896b2c08ccc95a38ee97a070.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4e1ae39f2ac00232f1bffad6f4b792",
+    "url": "/marvel/index.html"
+  },
+  {
+    "revision": "64c0f00617b043c55d96",
+    "url": "/marvel/static/css/main.00078dc6.chunk.css"
+  },
+  {
+    "revision": "a22950aa2f5b0925f236",
+    "url": "/marvel/static/js/2.1d3d1f2a.chunk.js"
+  },
+  {
+    "revision": "64c0f00617b043c55d96",
+    "url": "/marvel/static/js/main.45c0f8eb.chunk.js"
+  },
+  {
+    "revision": "03ccc9b3cbec2ecd8979",
+    "url": "/marvel/static/js/runtime~main.841ba7ac.js"
+  }
+]);
